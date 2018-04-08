4e630eaba3cc7c9156c82b9d03da53ebe12c8d81
94kazakov <94kazakov@gmail.com>
2018-04-08 00:40:09 -0600

    f1, precision, recall

--- tensorflow_helpers.py
+++ tensorflow_helpers.py
@@ -106,6 +106,73 @@ def project_into_output(Y, output, in_size, out_size, ops):
         Y_ = tf.nn.sigmoid(tf.matmul(output[-1], W_out) + b_out)
     return Y_
 
+
+"""
+Computes the F1 score on BIO tagged data
+
+@author: Nils Reimers
+"""
+
+
+def compute_f1(predictions, correct, idx2Label):
+    label_pred = [idx2Label[element] for element in predictions]
+
+    label_correct = [idx2Label[element] for element in correct]
+
+    # print label_pred
+    # print label_correct
+
+    prec = compute_precision(label_pred, label_correct)
+    rec = compute_precision(label_correct, label_pred)
+
+    f1 = 0
+    if (rec + prec) > 0:
+        f1 = 2.0 * prec * rec / (prec + rec)
+
+    return prec, rec, f1
+
+
+def compute_precision(guessed_sentences, correct_sentences):
+    assert (len(guessed_sentences) == len(correct_sentences))
+    correctCount = 0
+    count = 0
+
+    # for sentenceIdx in range(len(guessed_sentences)):
+    guessed = guessed_sentences
+    correct = correct_sentences
+    assert (len(guessed) == len(correct))
+    idx = 0
+    while idx < len(guessed):
+        if guessed[idx][0] == 'B':  # A new chunk starts
+            count += 1
+
+            if guessed[idx] == correct[idx]:
+                idx += 1
+                correctlyFound = True
+
+                while idx < len(guessed) and guessed[idx][0] == 'I':  # Scan until it no longer starts with I
+                    if guessed[idx] != correct[idx]:
+                        correctlyFound = False
+
+                    idx += 1
+
+                if idx < len(guessed):
+                    if correct[idx][0] == 'I':  # The chunk in correct was longer
+                        correctlyFound = False
+
+                if correctlyFound:
+                    correctCount += 1
+            else:
+                idx += 1
+        else:
+            idx += 1
+
+    precision = 0
+    if count > 0:
+        precision = float(correctCount) / count
+
+    return precision
+
 #
 # ATTRACTOR NETWORK:
 #
